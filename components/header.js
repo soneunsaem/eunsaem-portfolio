e1aaabb754ecebfd75f6af52ca8e4091188b372c
soneunsaem <97150057+soneunsaem@users.noreply.github.com>
2022-04-11 12:06:30 +0900

    Add files via upload

---
+++ components/header.js
@@ -0,0 +1,39 @@
+// 자바스크립트로 생성된 html components
+function header () {
+    return `
+    <header id="main-header">
+        <div class="main-header-bar container">
+            <h1>
+                <a href="index.html">
+                    <img src="./images/logo.svg" alt="로고">
+                </a>
+            </h1>
+                <div class="toggle-menu">
+                    <a class="toggle-menu-btn mobile-only">
+                        <img src="./images/toggle-menu-btn.svg" alt="모바일메뉴버튼">
+                    </a>
+                </div>
+                    <nav id="gnb">
+                        <ul>
+                            <li>
+                                <a href="about.html"> ABOUT </a>
+                            </li>
+                            <li>
+                                <a href="skills.html"> SKILLS </a>
+                            </li>
+                            <li>
+                                <a href="portfolio-home.html"> PORTFOLIO </a>
+                            </li>
+                            <li>
+                                <a href="contact.html"> CONTACT </a>
+                            </li>
+                        </ul>
+                    </nav>
+            </div>
+    </header>
+    `
+ }
+  
+  // 문서에 html 생성
+  const headerName = '.header-component'; // class 요소명
+  document.querySelector(headerName).innerHTML = header();
