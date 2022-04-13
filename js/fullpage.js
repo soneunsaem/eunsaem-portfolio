84fcc0722e30d51f0ebe5b0032d562b5447091a7
soneunsaem <97150057+soneunsaem@users.noreply.github.com>
2022-04-13 13:45:48 +0900

    풀스크린 JS 수정

---
+++ js/fullpage.js
@@ -0,0 +1,12 @@
+// 포트폴리오 소개 풀페이지
+
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+      //options here
+      anchors: ['firstPage', 'secondPage', 'thirdPage'],
+      menu: '#enu',
+      navigation: true,
+      navigationTooltips: ['PC', 'MOBILE', '반응형'],
+      navigationPosition: 'right',
+    });
+  });
--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-// 포트폴리오 소개 풀페이지
-
-$(document).ready(function () {
-    $('#fullpage').fullpage({
-      //options here
-      anchors: ['firstPage', 'secondPage', 'thirdPage'],
-      menu: '#enu',
-      navigation: true,
-      navigationTooltips: ['PC', 'MOBILE', '반응형'],
-      navigationPosition: 'right',
-    });
-  });
-
   $(function(){
     $('.toggle-menu-btn').click(function(){
       $('#gnb').toggleClass('on');
